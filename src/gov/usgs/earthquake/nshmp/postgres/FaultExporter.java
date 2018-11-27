b70a768e88876feef6c856034053afa64e779183
bclayton-usgs <bclayton@usgs.gov>
2018-11-27 07:30:52 -0700

    Exports GeoJSON

--- src/gov/usgs/earthquake/nshmp/postgres/FaultExporter.java
+++ src/gov/usgs/earthquake/nshmp/postgres/FaultExporter.java
@@ -1,176 +1,205 @@
 package gov.usgs.earthquake.nshmp.postgres;
 
-import static gov.usgs.earthquake.nshmp.postgres.sources.SourceAttribute.*;
+import static gov.usgs.earthquake.nshmp.postgres.Util.POSTGRES;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.BIRD_RAKE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.BIRD_RATE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.CALC_WIDTH;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.DEPTH;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.DIP;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.FAULT_TRACE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.GEO_RAKE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.GEO_RATE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.ID;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.NAME;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.PRIMARY_STATE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.RAKE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.SLIP_RATES;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.STATE_ABBREV;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.TITLE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.UPPER_DEPTH;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.WIDTH;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.WKT_FAULT_TRACE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.ZENG_RAKE;
+import static gov.usgs.earthquake.nshmp.postgres.Util.Keys.ZENG_RATE;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableList;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 
 import gov.usgs.earthquake.nshmp.geo.LocationList;
+import gov.usgs.earthquake.nshmp.geo.json.Feature;
+import gov.usgs.earthquake.nshmp.geo.json.GeoJson;
+import gov.usgs.earthquake.nshmp.geo.json.Properties;
 import gov.usgs.earthquake.nshmp.internal.UsRegion;
-import gov.usgs.earthquake.nshmp.postgres.source_settings.DeformationModel;
-import gov.usgs.earthquake.nshmp.postgres.source_settings.DeformationModelList;
-import gov.usgs.earthquake.nshmp.postgres.source_settings.SourceGeometry.FaultGeometry;
-import gov.usgs.earthquake.nshmp.postgres.sources.FaultSource;
-import gov.usgs.earthquake.nshmp.postgres.sources.FaultSourceSet;
-import gov.usgs.earthquake.nshmp.postgres.sources.FaultXMLExporter;
-import gov.usgs.earthquake.nshmp.postgres.sources.SourceAttribute;
 
 /**
- * Using {@link ModelParameters}, {@link Postgres}, {@link SourceAttribute},
- *    and {@link XMLExporter}, print out XML files for each U.S. state
- *    in the PostgreSQL database that is specified in {@link Postgres#builder()}
- *    
+ * Query a PostgreSQL fault database and write a GeoJSON file for each U.S.
+ * state.
+ * 
+ * <p> To run the main method: Must have a config.properties file in the root
+ * source directory with the following fields: 
+ *  <ul> 
+ *    <li> database: database to query </li>
+ *    <li> password: password for database </li>
+ *    <li> table: table name in database to query </li>
+ *    <li> url: Database url of form jdbc:postgresql:host:port </li>
+ *    <li> username: Database username </li>
+ * </ul>
+ * 
  * @author Brandon Clayton
- * @see XMLExporter#writeXML
  */
 public class FaultExporter {
 
-  public static void main(String[] args)  {
-    String configFile = "config.properties";
-    Postgres postgres = Postgres.builder()
-        .withConfigFile(configFile)
-        .build();
-   
+  public static void main(String[] args) {
+    Path out = Paths.get("output", POSTGRES.table());
+    export(POSTGRES, out);
+  }
+
+  /**
+   * Query the PostgreSQL database and write a GeoJSON file for each U.S. state.
+   * 
+   * @param postgres PostgreSQL to query
+   * @param out Output path for GeoJSON files
+   */
+  static void export(PostgreSQL postgres, Path out) {
     try {
       postgres.connect();
-      ArrayList<String> distinctStates = getDistinctStates(postgres);
-     
-      System.out.println("Creating XML for: ");
-      for (String stateAbbrev : distinctStates) {
-        String primaryState = UsRegion.valueOf(stateAbbrev).toString();
-        System.out.println(primaryState);
-        
-        String sql = "select " + getSqlSelectFields() +
-            " from " + postgres.table +
-            " where " + STATE_ABBREV + "='" + stateAbbrev + "'" +  
-            " order by " + NAME + " asc;";
-        
-        ResultSet result = postgres.query(sql);
-        ImmutableList.Builder<FaultSource> sourceListBuilder = ImmutableList.builder();
-        
-        while (result.next()) {
-          /* Get locationList from well know text */
-          String faultTraceWkt = result.getString(FAULT_TRACE.toString());
-          LocationList trace = wktToLocationList(faultTraceWkt);
-          
-          /* Set fault geometry */
-          FaultGeometry geometry = FaultGeometry.builder()
-              .depth(result.getDouble(UPPER_DEPTH.toLowerCase()))
-              .dip(result.getDouble(DIP.toLowerCase()))
-              .rake(result.getDouble(GEO_RAKE.toLowerCase()))
-              .trace(trace)
-              .width(result.getDouble(CALC_WIDTH.toLowerCase()))
-              .build();
-          
-          double birdRate = result.getDouble(BIRD_DISPLACEMENT_RATE.toLowerCase());
-          double geoRate = result.getDouble(GEO_DISPLACEMENT_RATE.toLowerCase());
-          double zengRate = result.getDouble(ZENG_DISPLACEMENT_RATE.toLowerCase());
-          
-          DeformationModelList deformationModels = DeformationModelList.builder()
-              .add(DeformationModel.bird(birdRate))
-              .add(DeformationModel.geo(geoRate))
-              .add(DeformationModel.zeng(zengRate))
-              .build();
-          
-          FaultSource.Builder sourceBuilder = FaultSource.builder();
-          sourceBuilder.deformationModels(deformationModels)
-              .geometry(geometry)
-              .id("?")
-              .name(result.getString(NAME.toLowerCase()));
-          
-          sourceListBuilder.add(sourceBuilder.build());
-        }
-        result.close();  
-        
-        Settings settings = Settings.westernUS();
-        FaultSourceSet sourceSet = FaultSourceSet.builder()
-            .defaultMfds(settings.defaultMfds)
-            .magUncertainties(settings.magUncertainties)
-            .name(primaryState + " " + FAULTS.toUpperCamelCase())
-            .ruptureScalingModels(settings.ruptureScalingModels)
-            .sources(sourceListBuilder.build())
-            .build();
-   
-        FaultXMLExporter.writeXML(sourceSet, postgres.table, primaryState);
+      Set<String> states = getDistinctStates(postgres);
+
+      System.out.println("Creating GeoJSON for: ");
+
+      for (String stateAbbrev : states) {
+        String state = UsRegion.valueOf(stateAbbrev).toString();
+        System.out.println(state);
+
+        ResultSet result = queryFault(postgres, stateAbbrev);
+        GeoJson.Builder geojson = resultToFeatureCollection(result);
+        result.close();
+        geojson.write(out.resolve(state + ".geojson"));
       }
-      
+
       postgres.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
-  
-  /**
-   * Query the PostgreSQL database using {@link Postgres#query(String)} and
-   *    obtain the distinct primart states.
-   *    
-   * @param postgres
-   * @return Array list of primary states.
-   * @throws SQLException
-   */
-  static ArrayList<String> getDistinctStates (Postgres postgres) 
+
+  /* Create a list of states that are in the database */
+  private static Set<String> getDistinctStates(PostgreSQL postgres)
       throws SQLException {
-    String sql = "select distinct " + STATE_ABBREV.toString() + 
-        " from " + postgres.table + 
-        " order by " + STATE_ABBREV.toString() + " asc";
-    ResultSet result = postgres.query(sql);
-    ArrayList<String> states = new ArrayList<>();
+
+    ResultSet result = PostgreSQL.queryBuilder()
+        .selectDistinct(STATE_ABBREV)
+        .from(postgres.table())
+        .orderByAscend(STATE_ABBREV)
+        .query(postgres);
+
+    Set<String> states = new TreeSet<>();
     
     while (result.next()) {
-      states.add(result.getString(STATE_ABBREV.toString()));
+      states.add(result.getString(STATE_ABBREV));
     }
-    
+
     return states;
   }
-  
-  /**
-   * Return a string for the SQL select method. 
-   */
-  static String getSqlSelectFields () {
-    ArrayList<String> selectFields = new ArrayList<>();
-    
-    selectFields.add(BIRD_DISPLACEMENT_RATE.toLowerCase());
-    selectFields.add(CALC_WIDTH.toLowerCase());
-    selectFields.add(DIP.toLowerCase());
-    selectFields.add(GEO_RAKE.toLowerCase());
-    selectFields.add(GEO_DISPLACEMENT_RATE.toLowerCase());
-    selectFields.add(NAME.toLowerCase());
-    selectFields.add(PRIMARY_STATE.toLowerCase());
-    selectFields.add("ST_AsText(" + FAULT_TRACE.toLowerCase() + ") as " + 
-        FAULT_TRACE.toLowerCase());
-    selectFields.add(STATE_ABBREV.toLowerCase());
-    selectFields.add(UPPER_DEPTH.toLowerCase());
-    selectFields.add(ZENG_DISPLACEMENT_RATE.toLowerCase());
-     
-    String select = selectFields.toString()
-        .replace("[", " ")
-        .replace("]", " ")
-        .trim();
-    
-    return select;
+
+  /* Query fault database */
+  private static ResultSet queryFault(PostgreSQL postgres, String stateAbbrev) throws SQLException {
+    return PostgreSQL.queryBuilder()
+        .select(getSQLSelectFields())
+        .from(postgres.table())
+        .where(STATE_ABBREV + "='" + stateAbbrev + "'")
+        .orderByAscend(NAME)
+        .query(postgres);
   }
-  
-  /**
-   * Return a {@code LocationList} of the fault trace.
-   * @param wkt The well know text.
-   * @throws ParseException
-   */
-  static LocationList wktToLocationList (String wkt) throws ParseException {
+
+  /* Create string of all fields to query */
+  private static String getSQLSelectFields() {
+    List<String> selectFields = new ArrayList<>();
+
+    selectFields.add(BIRD_RATE);
+    selectFields.add(BIRD_RAKE);
+    selectFields.add(CALC_WIDTH);
+    selectFields.add(DIP);
+    selectFields.add(GEO_RATE);
+    selectFields.add(GEO_RAKE);
+    selectFields.add(ID);
+    selectFields.add(NAME);
+    selectFields.add(PRIMARY_STATE);
+    selectFields.add(STATE_ABBREV);
+    selectFields.add(UPPER_DEPTH);
+    selectFields.add(WKT_FAULT_TRACE);
+    selectFields.add(ZENG_RATE);
+    selectFields.add(ZENG_RAKE);
+
+    return selectFields.stream().collect(Collectors.joining(","));
+  }
+
+  /* Convert the query result to a feature collection */
+  private static GeoJson.Builder resultToFeatureCollection(ResultSet result)
+      throws SQLException, ParseException {
+
+    GeoJson.Builder builder = GeoJson.builder();
+
+    while (result.next()) {
+      builder.add(resultToFeature(result));
+    }
+
+    return builder;
+  }
+
+  /* Convert the query to a feature */
+  private static Feature resultToFeature(ResultSet result) throws ParseException, SQLException {
+    String faultTraceWkt = result.getString(FAULT_TRACE);
+    LocationList trace = wktToLocationList(faultTraceWkt);
+
+    Map<String, Object> properties = Properties.builder()
+        .put(TITLE, result.getString(NAME))
+        .put(DEPTH, result.getDouble(UPPER_DEPTH))
+        .put(DIP, result.getDouble(DIP))
+        .put(RAKE, result.getDouble(GEO_RAKE))
+        .put(WIDTH, result.getDouble(CALC_WIDTH))
+        .put(SLIP_RATES, getSlipRates(result))
+        .build();
+
+    return Feature.polygon(trace)
+        .id(result.getInt(ID))
+        .properties(properties)
+        .build();
+  }
+
+  /* Create the slip rates */
+  private static List<SlipRate> getSlipRates(ResultSet result) throws SQLException {
+    return SlipRate.builder()
+        .bird(result.getDouble(BIRD_RAKE), result.getDouble(BIRD_RATE))
+        .geo(result.getDouble(GEO_RAKE), result.getDouble(GEO_RATE))
+        .zeng(result.getDouble(ZENG_RAKE), result.getDouble(ZENG_RATE))
+        .build();
+  }
+
+  /* Convert WKT to location list */
+  private static LocationList wktToLocationList(String wkt) throws ParseException {
     WKTReader wktReader = new WKTReader();
     Geometry faultTraceGeom = wktReader.read(wkt);
     LocationList.Builder trace = LocationList.builder();
+
     for (Coordinate coord : faultTraceGeom.getCoordinates()) {
-        trace.add(coord.y, coord.x, 0.0);
+      trace.add(coord.y, coord.x, 0.0);
     }
-    
+
     return trace.build();
   }
-  
+
 }
